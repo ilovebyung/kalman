4529119f225006f26ecf3e672b49001f86357305
ilovebyung <45321943+ilovebyung@users.noreply.github.com>
2020-11-12 00:31:45 +0900

    Update kalman.py
    VideoWriter is in progress

--- kalman.py
+++ kalman.py
@@ -1,12 +1,10 @@
-# --------------------------------------------------------------------
-# Implements motion prediction using Kalman Filter
-# --------------------------------------------------------------------
-
 import cv2
 import numpy as np
-import sys
+import datetime
 
-# Instantiate OCV kalman filter
+# --------------------------------------------------------------------
+# Kalman Filter predicts position and velocity
+# --------------------------------------------------------------------
 
 
 class KalmanFilter:
@@ -17,77 +15,83 @@ class KalmanFilter:
         [[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
 
     def estimate(self, x, y):
-        ''' This function estimates the position of the object'''
         measured = np.array([[np.float32(x)], [np.float32(y)]])
         self.kf.correct(measured)
         predicted = self.kf.predict()
         return predicted
 
-# Detect an object with BackgroundSubtractorKNN
 
-
+# set parameters for BackgroundSubtractorKNN
 bg_subtractor = cv2.createBackgroundSubtractorKNN(detectShadows=True)
-history_length = 100
+history_length = 20
 bg_subtractor.setHistory(history_length)
 erode_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 5))
 dilate_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (17, 11))
 
-# Performs required image processing to get object coordinated in the video
+# capture input
 cap = cv2.VideoCapture('heron.mp4')
+success, frame = cap.read()
 
-if(cap.isOpened() == False):
-    print('Cannot open input')
-    pass
-
-# width = int(cap.get(3))
-# height = int(cap.get(4))
+# Define the codec and create VideoWriter object
+fshape = frame.shape
+fheight = fshape[0]
+fwidth = fshape[1]
+print(f'Width:{fwidth}, Height:{fheight}')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
 # Create Kalman Filter Object
 kalman = KalmanFilter()
-predictedCoords = np.zeros((2, 1), np.float32)
+predictions = np.zeros((2, 1), np.float32)
 
+while success:
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+    # --------------------------------------------------------------------
+    # BackgroundSubtractorKNN detects moving objects
+    # --------------------------------------------------------------------
 
-    if(ret == True):
-        fg_mask = bg_subtractor.apply(frame)
-        _, thresh = cv2.threshold(fg_mask, 244, 255, cv2.THRESH_BINARY)
-        cv2.erode(thresh, erode_kernel, thresh, iterations=2)
-        cv2.dilate(thresh, dilate_kernel, thresh, iterations=2)
+    fg_mask = bg_subtractor.apply(frame)
+    _, thresh = cv2.threshold(fg_mask, 244, 255, cv2.THRESH_BINARY)
+    cv2.erode(thresh, erode_kernel, thresh, iterations=2)
+    cv2.dilate(thresh, dilate_kernel, thresh, iterations=2)
 
-        contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
-                                          cv2.CHAIN_APPROX_SIMPLE)
+    contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
+                                      cv2.CHAIN_APPROX_SIMPLE)
 
-        for c in contours:
-            if cv2.contourArea(c) > 2000:
-                x, y, w, h = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+    for c in contours:
+        # Set the valid size
+        if cv2.contourArea(c) > 2000:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
-                # [x, y] = detect(frame)
-                predictedCoords = kalman.estimate(x, y)
+            predictions = kalman.estimate(x, y)
 
-                # Draw Actual coords from segmentation
-                cv2.circle(frame, (int(x), int(y)),
-                           20, [0, 0, 255], 2, 8)
-                cv2.line(frame, (int(x), int(y + 20)),
-                         (int(x + 50), int(y + 20)), [100, 100, 255], 2, 8)
-                cv2.putText(frame, "Actual", (int(x + 50), int(y + 20)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, [50, 200, 250])
+            # Draw Actual coords from segmentation
+            cv2.circle(frame, (int(x), int(y)),
+                       20, [0, 0, 255], 2, 8)
+            cv2.line(frame, (int(x), int(y + 20)),
+                     (int(x + 50), int(y + 20)), [100, 100, 255], 2, 8)
+            cv2.putText(frame, "Actual", (int(x + 50), int(y + 20)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, [50, 200, 250])
 
-                # Draw Kalman Filter Predicted output
-                cv2.circle(frame, (int(predictedCoords[0]), int(predictedCoords[1])), 20, [
-                    0, 255, 255], 2, 8)
-                cv2.line(frame, (int(predictedCoords[0]) + 16, int(predictedCoords[1]) - 15),
-                         (int(predictedCoords[0]) + 50, int(predictedCoords[1]) - 30), [100, 10, 255], 2, 8)
-                cv2.putText(frame, "Predicted", (int(predictedCoords[0] + 50), int(
-                    predictedCoords[1] - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [50, 200, 250])
-                cv2.imshow('Input', frame)
+            # Draw Kalman Filter Predicted output
+            # cv2.circle(frame, (int(predictions[0]), int(predictions[1])), 20, [
+            #     0, 255, 255], 2, 8)
+            # cv2.line(frame, (int(predictions[0]) + 16, int(predictions[1]) - 15),
+            #          (int(predictions[0]) + 50, int(predictions[1]) - 30), [100, 10, 255], 2, 8)
+            cv2.putText(frame, "Predicted xv, yv", (int(predictions[2] + 50), int(
+                predictions[3] - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [50, 200, 250])
+            cv2.imshow('Input', frame)
 
-        if (cv2.waitKey(300) & 0xFF == ord('q')):
-            break
+            # Set file name as 'MM/DD/YY HH:MM:SS'
+            out = datetime.datetime.now().strftime("%x %X")
 
-    else:
+            # write the frame
+            out = cv2.VideoWriter(f'{out}.avi', fourcc,
+                                  20.0, (fwidth, fheight))
+            out.write(frame)
+
+    k = cv2.waitKey(30)
+    if k == 27:  # Escape
         break
 
 cap.release()
